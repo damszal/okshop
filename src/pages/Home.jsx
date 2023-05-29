121cab17e72633d6edfcd93beaf0efad52237d94
damian <damian.szalimow@onet.eu>
2023-05-29 08:17:38 +0200

    add grid layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
+import '../css/pages/home.css'
+
 function Home() {
   return (
-    <>
-    <h1>OK!shop - home page</h1>
-  </>
+    <div className='home-container'>
+      <div className='slider'>
+        <img src="https://placehold.co/900x360" alt="" />
+      </div>
+      <div className='main-banner'>
+      </div>
+      {/* <div className='mini-banners'> */}
+        <div className='mini-banner-01'>
+          <img src="https://placehold.co/200x200" alt="" />
+        </div>
+        <div className='mini-banner-02'>
+          <img src="https://placehold.co/200x200" alt="" />
+        </div>
+        <div className='mini-banner-03'>
+          <img src="https://placehold.co/200x200" alt="" />
+        </div>
+        <div className='mini-banner-04'>
+          <img src="https://placehold.co/200x200" alt="" />
+        </div>
+        <div className='mini-banner-05'>
+          <img src="https://placehold.co/200x200" alt="" />
+        </div>
+        <div className='mini-banner-06'>
+          <img src="https://placehold.co/200x200" alt="" />
+        </div>
+      {/* </div> */}
+    </div>
   )
 }
 
