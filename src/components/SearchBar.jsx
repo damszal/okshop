958bdeaa349d21a8747001eb401227f842924718
damian <damian.szalimow@onet.eu>
2023-06-02 03:40:39 +0200

    change the header layout

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import '../css/components/search-bar.css'
 
 function Searchbar() {
   return (
-    <>
-    <form action="">
-      <input ></input>
-      <input type="submit" value="Search" />
+    <form action="" className='search-bar'>
+      <input className='search-bar-input'
+      placeholder='type a product'
+      ></input>
+      <button className='search-bar-btn'
+      type='submit'
+      >search</button>
     </form>
-    </>
   )
 }
 
