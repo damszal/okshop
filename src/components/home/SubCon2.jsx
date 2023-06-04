11c9faac73abe7cb99375989bf364ea9b0026ef1
damian <damian.szalimow@onet.eu>
2023-06-04 03:09:27 +0200

    add images to  .home-sub-con-03

--- src/components/home/SubCon2.jsx
+++ src/components/home/SubCon2.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 
+import img from '../../assets/items/item2verysmall.png'
+
 export default function SubCon2() {
   return (
 <section className="home-sub-con-02">
     <div className='mini-banner-01'> 
-      <img src="https://placehold.co/200x200" alt="" />
+      <img src={img}alt="" />
     </div>
     <div className='mini-banner-02'>
-      <img src="https://placehold.co/200x200" alt="" />
+      <img src={img} alt="" />
     </div>
     <div className='mini-banner-03'>
-      <img src="https://placehold.co/200x200" alt="" />
+      <img src={img} alt="" />
     </div>
     <div className='mini-banner-04'>
-      <img src="https://placehold.co/200x200" alt="" />
+      <img src={img} alt="" />
     </div>
     <div className='mini-banner-05'>
-      <img src="https://placehold.co/200x200" alt="" />
+      <img src={img} alt="" />
     </div>
     <div className='mini-banner-06'>
-      <img src="https://placehold.co/200x200" alt="" />
+      <img src={img} alt="" />
     </div> 
 </section>
   )
