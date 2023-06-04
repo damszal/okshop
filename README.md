e066289593594a41b7b288680e5fb1137d17b151
damian <damian.szalimow@onet.eu>
2023-06-04 02:20:31 +0200

    add img to SubCon1 and up-to-date readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 OK!shop for dagon project
 
 host on https://stellar-starlight-074cd2.netlify.app/
+
+website is available on min width 1280px
--- src/components/home/SubCon1.jsx
+++ src/components/home/SubCon1.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import imgUrl from '../../assets/items/item1small.png'
 
 function SubCon1() {
   return (
     <section className='home-sub-con-01'>
         <div className='main-slider '>
-          <img src="https://placehold.co/900x360" alt="" />
+          <img src={imgUrl} alt="" />
         </div>
         <div className='main-banner'></div>
   </section>
