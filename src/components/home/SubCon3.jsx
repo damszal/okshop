e1899362ed52ba0c11fafc34f39edd92bf349ec9
damian <damian.szalimow@onet.eu>
2023-06-04 03:52:48 +0200

    add item3 img to assets folder and change a  .home-sub-con-03 class

--- src/components/home/SubCon3.jsx
+++ src/components/home/SubCon3.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import imgUrl from '../../assets/items/item3small.png'
 
 function SubCon3() {
   return (
     <section className='home-sub-con-03'>
         <div className="c-01-adv">
-          <img src="https://placehold.co/400x200" alt="" />
+          <h2>item name</h2>
+          <img src={imgUrl} alt="" />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
         </div>
         <div className="c-02-adv">
-          <img src="https://placehold.co/400x200" alt="" />
+          <h2>item name</h2>
+          <img src={imgUrl} alt="" />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
         </div>
         <div className="c-03-adv">
-          <img src="https://placehold.co/400x200" alt="" />
+          <h2>item name</h2>
+          <img src={imgUrl} alt="" />
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
         </div>
     </section>
