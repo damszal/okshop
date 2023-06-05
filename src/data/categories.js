d94a318224c104e2cd38cf60eb1018135a5fe357
damian <damian.szalimow@onet.eu>
2023-06-06 00:36:32 +0200

    add data folder

---
+++ src/data/categories.js
@@ -0,0 +1,18 @@
+let categories = 
+   [{
+    id : 1, 
+    name: "category01",
+    subcat: [{name:"subcat#1"},{name:"subcat#2"},{name:"subcat#3"},{name:"subcat#4"},]
+   },
+    {
+        id : 2, 
+        name: "category02",
+        subcat: [{name:"subcat#1"},{name:"subcat#2"},{name:"subcat#3"},{name:"subcat#4"},{name:"subcat#5"},]
+    },
+    {
+        id : 3, 
+        name: "category03",
+        subcat: [{name:"subcat#1"},{name:"subcat#2"}]
+    }
+   ]
+   export default categories
