7232b065753ad5fb18d788b240b75e999a89dde3
damian <damian.szalimow@onet.eu>
2023-06-15 01:30:45 +0200

    add picture tag to home-sub-con-01 class

--- src/components/home/SubCon1.jsx
+++ src/components/home/SubCon1.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import imgUrl from '../../assets/items/item1small.png'
+import imgUrlA from '../../assets/items/item1-A.png'
+import imgUrlB from '../../assets/items/item1-B.png'
+import imgUrlC from '../../assets/items/item1-C.png'
+import imgUrlD from '../../assets/items/item1-D.png'
+import imgUrlE from '../../assets/items/item1-E.png'
 
 function SubCon1({cat}) {
   return (
     <section className='home-sub-con-01'>
-        <div className='main-slider '>
-          <img src={imgUrl} alt="" />
-        </div>
+        <picture className='main-slider '>
+          <source media="(max-width: 475px)" srcset={imgUrlE} />
+          <source media="(max-width: 640px)" srcset={imgUrlD} />
+          <source media="(max-width: 768px)" srcset={imgUrlC} />
+          <source media="(max-width: 1024px)" srcset={imgUrlB} />
+          <img 
+          src={imgUrlA} 
+          />
+        </picture>
         <div className='main-banner'>
           {cat.map((e,i)=>{
             return (
