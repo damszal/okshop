b15bdf9dd82ae881d997c71c9ca2c214572ccb21
damian <damian.szalimow@onet.eu>
2023-06-16 23:24:43 +0200

    add a main nav manu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,12 @@ import SearchBar from './SearchBar.jsx';
 import Basket from './Basket.jsx';
 import Logotype from './logotype.jsx';
 
+import '../css/components/main-manu.scss'
+
 function Header() {
   return (
-    <header>
+  <>
+  <header>
     <Link to="/" ><Logotype /></Link>
     <SearchBar />
     <nav>
@@ -17,6 +20,67 @@ function Header() {
     </nav>
     <Basket />
   </header>
+  <manu>
+    <nav className='main-nav'>
+      <ul className='main-nav-list'>
+        <li className='main-nav-item'>
+          <h4>category 01</h4>
+          <div className='main-nav-submenu'>
+            <ul>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+            </ul>
+          </div>
+        </li>
+        <li className='main-nav-item'>
+          <h4>category 02</h4>
+          <div className='main-nav-submenu'>
+            <ul>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>           
+            </ul>
+          </div>
+        </li>
+        <li className='main-nav-item'>
+          <h4>category 03</h4>
+          <div className='main-nav-submenu'>
+            <ul>
+              <li>subcat</li>
+              <li>subcat</li>
+            </ul>
+          </div>
+        </li>
+        <li className='main-nav-item'>
+          <h4>category 04</h4>
+          <div className='main-nav-submenu'>
+            <ul>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+            </ul>
+          </div>
+          </li>
+        <li className='main-nav-item'>
+          <h4>category 05</h4>
+          <div className='main-nav-submenu'>
+            <ul>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+              <li>subcat</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  </manu> 
+  </>
   )
 }
 
