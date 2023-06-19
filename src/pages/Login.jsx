bce6221ae8b7ce40134d597b3c917a7fabcbbd0f
damian <damian.szalimow@onet.eu>
2023-06-20 00:35:05 +0200

    refactor login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import {useFormik} from 'formik'
 import * as Yup from 'yup'
 
+import {Grid,Typography,TextField, Button,
+} from '@mui/material'
+
 function Login() {
   const formik = useFormik({
     initialValues:{
-      login: '',
+      email: '',
       password: '',
    }, 
    validationSchema : Yup.object({
@@ -18,43 +21,93 @@ function Login() {
   })
 
   return (
-   <section className=''>
-    <form 
-    className=''
+    <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ minHeight: '70vh' }}
+    component="form"
     onSubmit={formik.handleSubmit}
-    >
-      <div>
-        <label htmlFor="login">login:</label>
-        <input 
-        placeholder='LOGIN' 
-        type="text" 
-        name="login" 
-        id="login" 
-        value={formik.values.login}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        />      
-        {formik.touched.login && formik.errors.login ? formik.errors.login: formik.values.login}
-      </div>
-      <div>
-        <label htmlFor="pass">password:</label>
-        <input 
-        placeholder='PASSWORD' 
-        type="text" 
-        name="password" 
-        id="password" 
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        />      
+  >
+    <Grid item style={{ border: "0.2px solid gray" , padding:"2rem"}}>
+      <Grid container direction="column" alignItems="center" justify="center">
+        <TextField
+          variant="outlined"
+          label="Email"
+          fullWidth
+          style={{ marginBottom: "2em" }}
+          type="text" 
+          name="email" 
+          id="email" 
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.login && formik.errors.email ? formik.errors.email: formik.values.email}
+        <TextField
+          variant="outlined"
+          label="Password"
+          fullWidth
+          style={{ marginBottom: "2em" }}
+          type="text" 
+          name="password" 
+          id="password" 
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
         {formik.touched.password && formik.errors.password ? formik.errors.password: formik.values.password}
-      </div>
-      <button 
-      type='submit'
-      >
-        Login</button>
-    </form>
-   </section>
+        <Button size="large" variant="contained" color="primary" type='submit'>
+          sign in
+        </Button>
+      </Grid>
+    </Grid>
+  </Grid>
+
+
+
+
+
+
+  //  <section className=''>
+  //   <form 
+  //   className=''
+  //   onSubmit={formik.handleSubmit}
+  //   >
+  //     <div>
+  //       <label htmlFor="login">login:</label>
+  //       <input 
+  //       placeholder='LOGIN' 
+  //       type="text" 
+  //       name="login" 
+  //       id="login" 
+  //       value={formik.values.login}
+  //       onChange={formik.handleChange}
+  //       onBlur={formik.handleBlur}
+  //       />      
+  //       {formik.touched.login && formik.errors.login ? formik.errors.login: formik.values.login}
+  //     </div>
+  //     <div>
+  //       <label htmlFor="pass">password:</label>
+  //       <input 
+  //       placeholder='PASSWORD' 
+  //       type="text" 
+  //       name="password" 
+  //       id="password" 
+  //       value={formik.values.password}
+  //       onChange={formik.handleChange}
+  //       onBlur={formik.handleBlur}
+  //       />      
+  //       {formik.touched.password && formik.errors.password ? formik.errors.password: formik.values.password}
+  //     </div>
+  //     <button 
+  //     type='submit'
+  //     >
+  //       Login</button>
+  //   </form>
+  //  </section>
   )
 }
 
