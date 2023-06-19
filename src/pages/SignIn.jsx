32f4dd07c3bb622c9666b594093ce918ad4b0d05
damian <damian.szalimow@onet.eu>
2023-06-20 00:01:43 +0200

    add materia ui module, make sign in form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import * as React from 'react';
+
+import {Grid,Typography,TextField, Button,
+} from '@mui/material'
+
+
 
 function SignIn() {
   return (
-    <div>signin page</div>
+    <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ minHeight: '70vh' }}
+  >
+    <Grid item style={{ border: "0.2px solid gray" , padding:"2rem"}}>
+      <Grid container direction="column" alignItems="center" justify="center">
+        <TextField
+          variant="outlined"
+          label="Email"
+          fullWidth
+          style={{ marginBottom: "2em" }}
+        />
+        <TextField
+          variant="outlined"
+          label="Password"
+          fullWidth
+          style={{ marginBottom: "2em" }}
+        />
+        <TextField
+          variant="outlined"
+          label="Repeat Password"
+          fullWidth
+          style={{ marginBottom: "2em" }}
+        />
+        <Button size="large" variant="contained" color="primary">
+          sign in
+        </Button>
+      </Grid>
+    </Grid>
+  </Grid>
   )
 }
+
 
 export default SignIn
