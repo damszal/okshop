10d995f5fd08e6d6f33b6ccad9e68df5026c076a
damian <damian.szalimow@onet.eu>
2023-06-20 19:12:11 +0200

    add express - basic setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'server/build'
+  },
+  // use when want to check all react routers functionality
+  // server: {
+  //   port: 8080,
+  // },
 })
