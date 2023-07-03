590ac24f508935cd1c206c7c9cdfa7902c376c4f
damian <damian.szalimow@onet.eu>
2023-07-03 23:01:00 +0200

    create SubCon3Img component

--- src/components/home/SubCon3.jsx
+++ src/components/home/SubCon3.jsx
@@ -1,59 +1,27 @@
-import React from 'react'
-import imgUrl from '../../assets/items/item3small.png'
-
-import { Outlet,Link } from "react-router-dom";
+import SubCon3Img from './SubCon3Img';
+import { Outlet} from "react-router-dom";
 
 function SubCon3() {
   return (
     <>
     <section className='home-sub-con-03'>
         <div className="c-01-adv home-sub-con-3-item">
-          <h2>item name</h2>
-          <img src={imgUrl} alt="" />
-            <button className='show-item-btn'>
-              <Link to="/cart/item">show item</Link>
-            </button>     
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
+          <SubCon3Img />  
         </div>
         <div className="c-02-adv home-sub-con-3-item">
-          <h2>item name</h2>
-          <img src={imgUrl} alt="" />
-          <button className='show-item-btn'>
-              <Link to="/cart/item">show item</Link>
-          </button> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
+          <SubCon3Img />  
         </div>
         <div className="c-03-adv home-sub-con-3-item">
-          <h2>item name</h2>
-          <img src={imgUrl} alt="" />
-          <button className='show-item-btn'>
-              <Link to="/cart/item">show item</Link>
-          </button> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
+        <SubCon3Img />  
         </div>
         <div className="c-04-adv home-sub-con-3-item">
-          <h2>item name</h2>
-          <img src={imgUrl} alt="" />
-          <button className='show-item-btn'>
-              <Link to="/cart/item">show item</Link>
-          </button> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
+          <SubCon3Img />  
         </div>
         <div className="c-05-adv home-sub-con-3-item">
-          <h2>item name</h2>
-          <img src={imgUrl} alt="" />
-          <button className='show-item-btn'>
-              <Link to="/cart/item">show item</Link>
-          </button> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
+        <SubCon3Img />  
         </div>
         <div className="c-06-adv home-sub-con-3-item">
-          <h2>item name</h2>
-          <img src={imgUrl} alt="" />
-          <button className='show-item-btn'>
-              <Link to="/cart/item">show item</Link>
-          </button> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>
+          <SubCon3Img />  
         </div>
     </section>
     <Outlet/>
---
+++ src/components/home/SubCon3Img.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import imgUrl from '../../assets/items/item3small.png'
+import { Link } from "react-router-dom";
+
+function SubCon3Img() {
+  return (
+    <>
+    <h2>item name</h2>
+    <img src={imgUrl} alt="" />
+      <button className='show-item-btn'>
+        <Link to="/cart/item">show item</Link>
+      </button>     
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>  
+    </>
+  )
+}
+
+export default SubCon3Img
