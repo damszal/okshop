13fda7cf381ef412c407c94bbfb2ef31798b1402
damian <damian.szalimow@onet.eu>
2023-08-27 18:02:35 +0200

    add sql2 module

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 require('dotenv').config()
 const express = require("express");
 const usersRout = require('./routes/users')
+
 const app = express();
+const mysql = require('mysql2');
 
 const PORT =  process.env.PORT;
 
 app.listen(PORT, () => console.log("Server started on port " + process.env.PORT));
 
+const connection = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    port: '3306',
+    user: 'root',
+    password: process.env.SQL_PASS,
+  });
+
+  connection.query(
+    'SELECT * FROM products.product;',
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+      console.log(err); 
+    }
+  );
 
 app.use('/api', usersRout);
