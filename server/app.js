c3649647d3a4593924c97d69934d195615c9cea5
damian <damian.szalimow@onet.eu>
2023-08-27 17:04:19 +0200

    add .env module

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require("express");
 const usersRout = require('./routes/users')
 const app = express();
 
-const PORT =  8080;
+const PORT =  process.env.PORT;
 
-app.listen(PORT, () => console.log("Server started"));
+app.listen(PORT, () => console.log("Server started on port " + process.env.PORT));
 
 
 app.use('/api', usersRout);
