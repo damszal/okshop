5b02329952a1b42384b0a3c00388dcca8765729c
damian <damian.szalimow@onet.eu>
2023-08-30 15:04:20 +0200

    copy login code from about page to login page, temporary delete formik and yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import {useFormik} from 'formik'
-import * as Yup from 'yup'
+import React, {useState} from 'react'
+import axios from 'axios'
 
-import {Grid,Typography,TextField, Button,
+import {Grid,TextField, Button,
 } from '@mui/material'
 
 function Login() {
-  const formik = useFormik({
-    initialValues:{
-      email: '',
-      password: '',
-   }, 
-   validationSchema : Yup.object({
-     login: Yup.string().max(10,"too much characters!").required("type login"),
-     password: Yup.string().max(10,"too much characters!").required("type password")
-    }),
-    onSubmit : (values) => {
-     console.log(values)
-    },
-  })
+  
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginMsg, setLoginMsg] = useState('')
+  
+  const login = (e) => {
+    e.preventDefault();
+    axios.post('http://127.0.0.1:5173/log/loged', {
+      username : email,
+      password : password,
+    }).then((response) => {
+      console.log(response)
+      if(response.data.message){
+        setLoginMsg(response.data.message)
+      }else {
+        setLoginMsg(response.data[0].Email)
+      }
+    })
+  }
 
   return (
     <Grid
@@ -29,7 +34,6 @@ function Login() {
     justifyContent="center"
     sx={{ minHeight: '70vh' }}
     component="form"
-    onSubmit={formik.handleSubmit}
   >
     <Grid item style={{ border: "0.2px solid gray" , padding:"2rem"}}>
       <Grid container direction="column" alignItems="center" justify="center">
@@ -41,11 +45,12 @@ function Login() {
           type="text" 
           name="email" 
           id="email" 
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          onChange={(e)=>{
+            setEmail(e.target.value)
+          }}
+
         />
-        {formik.touched.login && formik.errors.email ? formik.errors.email: formik.values.email}
+
         <TextField
           variant="outlined"
           label="Password"
@@ -54,14 +59,18 @@ function Login() {
           type="text" 
           name="password" 
           id="password" 
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }} 
+
         />
-        {formik.touched.password && formik.errors.password ? formik.errors.password: formik.values.password}
-        <Button size="large" variant="contained" color="primary" type='submit'>
+
+        <Button size="large" variant="contained" color="primary" type='submit' onClick={login}>
           sign in
         </Button>
+        <p>
+          {loginMsg}
+        </p>
       </Grid>
     </Grid>
   </Grid>
@@ -69,45 +78,68 @@ function Login() {
 
 
 
+// formik and yup module are still instaled 
 
+// function Login() {
+//   const formik = useFormik({
+//     initialValues:{
+//       email: '',
+//       password: '',
+//    }, 
+//    validationSchema : Yup.object({
+//      login: Yup.string().max(10,"too much characters!").required("type login"),
+//      password: Yup.string().max(10,"too much characters!").required("type password")
+//     }),
+//     onSubmit : (values) => {
+//      console.log(values)
+//     },
+//   })
 
-  //  <section className=''>
-  //   <form 
-  //   className=''
-  //   onSubmit={formik.handleSubmit}
-  //   >
-  //     <div>
-  //       <label htmlFor="login">login:</label>
-  //       <input 
-  //       placeholder='LOGIN' 
-  //       type="text" 
-  //       name="login" 
-  //       id="login" 
-  //       value={formik.values.login}
-  //       onChange={formik.handleChange}
-  //       onBlur={formik.handleBlur}
-  //       />      
-  //       {formik.touched.login && formik.errors.login ? formik.errors.login: formik.values.login}
-  //     </div>
-  //     <div>
-  //       <label htmlFor="pass">password:</label>
-  //       <input 
-  //       placeholder='PASSWORD' 
-  //       type="text" 
-  //       name="password" 
-  //       id="password" 
-  //       value={formik.values.password}
-  //       onChange={formik.handleChange}
-  //       onBlur={formik.handleBlur}
-  //       />      
-  //       {formik.touched.password && formik.errors.password ? formik.errors.password: formik.values.password}
-  //     </div>
-  //     <button 
-  //     type='submit'
-  //     >
-  //       Login</button>
-  //   </form>
-  //  </section>
+//   return (
+//     <Grid
+//     container
+//     spacing={0}
+//     direction="column"
+//     alignItems="center"
+//     justifyContent="center"
+//     sx={{ minHeight: '70vh' }}
+//     component="form"
+//     onSubmit={formik.handleSubmit}
+//   >
+//     <Grid item style={{ border: "0.2px solid gray" , padding:"2rem"}}>
+//       <Grid container direction="column" alignItems="center" justify="center">
+//         <TextField
+//           variant="outlined"
+//           label="Email"
+//           fullWidth
+//           style={{ marginBottom: "2em" }}
+//           type="text" 
+//           name="email" 
+//           id="email" 
+//           value={formik.values.email}
+//           onChange={formik.handleChange}
+//           onBlur={formik.handleBlur}
+//         />
+//         {formik.touched.login && formik.errors.email ? formik.errors.email: formik.values.email}
+//         <TextField
+//           variant="outlined"
+//           label="Password"
+//           fullWidth
+//           style={{ marginBottom: "2em" }}
+//           type="text" 
+//           name="password" 
+//           id="password" 
+//           value={formik.values.password}
+//           onChange={formik.handleChange}
+//           onBlur={formik.handleBlur}
+//         />
+//         {formik.touched.password && formik.errors.password ? formik.errors.password: formik.values.password}
+//         <Button size="large" variant="contained" color="primary" type='submit'>
+//           sign in
+//         </Button>
+//       </Grid>
+//     </Grid>
+//   </Grid>
   )
 }
 
