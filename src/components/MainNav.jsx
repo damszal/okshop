04a6d0c0a700df71b2d79b6e49da06cb9bb3dd22
damian <damian.szalimow@onet.eu>
2023-09-13 13:54:41 +0200

    add mobile manu label

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,67 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MainNav() {
+  const [fullPage, setFullPage] = useState(false)
+
+  const  manuHandler = () =>{
+    return setFullPage((fullPage)=>!fullPage )
+  }
   return (
-        <nav className='main-nav'>
-      <ul className='main-nav-list'>
-        <li className='main-nav-item'>
-          <h4>MEN</h4>
-          <div className='main-nav-submenu'>
-            <ul>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-            </ul>
-          </div>
-        </li>
-        <li className='main-nav-item'>
-          <h4>WOMEN</h4>
-          <div className='main-nav-submenu'>
-            <ul>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>           
-            </ul>
-          </div>
-        </li>
-        <li className='main-nav-item'>
-          <h4>KIDS</h4>
-          <div className='main-nav-submenu'>
-            <ul>
-              <li>subcat</li>
-              <li>subcat</li>
-            </ul>
-          </div>
-        </li>
-        <li className='main-nav-item'>
-          <h4>LIMITED EDITION</h4>
-          <div className='main-nav-submenu'>
-            <ul>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-            </ul>
-          </div>
+    <>
+      <div className={fullPage?'manu-640-full':'manu-640-container'}  
+      >
+        <span 
+        className='manu-640-btn'
+        onClick={manuHandler}
+        >
+          PICK YOUR SHOES
+        </span>
+      </div>
+      <nav className='main-nav'>
+        <ul className='main-nav-list'>
+          <li className='main-nav-item'>
+            <h4>MEN</h4>
+            <div className='main-nav-submenu'>
+              <ul>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+              </ul>
+            </div>
           </li>
-        <li className='main-nav-item'>
-          <h4>OUTLET</h4>
-          <div className='main-nav-submenu'>
-            <ul>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-              <li>subcat</li>
-            </ul>
-          </div>
-        </li>
-      </ul>
+          <li className='main-nav-item'>
+            <h4>WOMEN</h4>
+            <div className='main-nav-submenu'>
+              <ul>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>           
+              </ul>
+            </div>
+          </li>
+          <li className='main-nav-item'>
+            <h4>KIDS</h4>
+            <div className='main-nav-submenu'>
+              <ul>
+                <li>subcat</li>
+                <li>subcat</li>
+              </ul>
+            </div>
+          </li>
+          <li className='main-nav-item'>
+            <h4>LIMITED EDITION</h4>
+            <div className='main-nav-submenu'>
+              <ul>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+              </ul>
+            </div>
+            </li>
+          <li className='main-nav-item'>
+            <h4>OUTLET</h4>
+            <div className='main-nav-submenu'>
+              <ul>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+                <li>subcat</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
     </nav>
-    
-    
+   </>   
   )
 }
 
