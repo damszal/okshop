5bfb24e4e1dfd3e443b7e96c2e426b49bbf8896a
damian <damian.szalimow@onet.eu>
2023-09-19 17:08:24 +0200

    refactor scss main file

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,7 @@ import { Outlet } from "react-router-dom";
 
 import Header from './components/Header.jsx';
 
-import './App.scss'
-import './css/components/header.scss'
-import './css/components/search-bar.scss'
-import './css/components/logotype.scss'
-import './css/components/main-manu.scss'
-import './css/components/footer.scss'
-import './css/pages/home.scss'
-import './css/pages/about.scss'
-import './css/pages/sideCart.scss'
-import './css/mediaQuery/resolution.scss'
-
-
+import './All.scss'
 
 function App() {
 
