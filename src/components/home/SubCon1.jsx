bcd24f5e56ebaceafd45b6bc53aabc842cdc1fea
damian <damian.szalimow@onet.eu>
2023-09-19 23:25:34 +0200

    refactor to react element

--- src/components/home/SubCon1.jsx
+++ src/components/home/SubCon1.jsx
@@ -1,50 +1,33 @@
 import React from 'react'
+import ImageView from './SubCon1/ImageView'
+
 import imgUrlA from '../../assets/items/item1-A.png'
 import imgUrlB from '../../assets/items/item1-B.png'
 import imgUrlC from '../../assets/items/item1-C.png'
 import imgUrlD from '../../assets/items/item1-D.png'
 import imgUrlE from '../../assets/items/item1-E.png'
 
-import { Outlet,Link } from "react-router-dom";
+const imgUrls = {
+  urlA : imgUrlA,
+  urlB : imgUrlB,
+  urlC : imgUrlC,
+  urlD : imgUrlD,
+  urlE : imgUrlE,
+}
 
 
-
-function SubCon1({cat}) {
+function SubCon1() {
   return (
     <section className='home-sub-con-01'>
-        <picture className='main-img main-img-01'>
-          <Link to="/cart/item">
-            <source media="(max-width: 475px)" srcSet={imgUrlD} />
-            <source media="(max-width: 640px)" srcSet={imgUrlC} />
-            <source media="(max-width: 768px)" srcSet={imgUrlC} />
-            <source media="(max-width: 1024px)" srcSet={imgUrlB} />
-            <img 
-            src={imgUrlB} 
-            />
-          </Link>
-        </picture>      
-        <picture className='main-img main-img-02'>
-          <Link to="/cart/item">
-            <source media="(max-width: 475px)" srcSet={imgUrlD} />
-            <source media="(max-width: 640px)" srcSet={imgUrlC} />
-            <source media="(max-width: 768px)" srcSet={imgUrlC} />
-            <source media="(max-width: 1024px)" srcSet={imgUrlB} />
-            <img 
-            src={imgUrlB} 
-            />
-          </Link>
-        </picture>
-        <picture className='main-img main-img-03'>
-        <Link to="/cart/item">
-            <source media="(max-width: 475px)" srcSet={imgUrlD} />
-            <source media="(max-width: 640px)" srcSet={imgUrlC} />
-            <source media="(max-width: 768px)" srcSet={imgUrlC} />
-            <source media="(max-width: 1024px)" srcSet={imgUrlB} />
-            <img 
-            src={imgUrlB} 
-            />
-        </Link>
-        </picture>
+      <picture className='main-img main-img-01'>
+        <ImageView urlAll = {imgUrls} />
+      </picture>
+      <picture className='main-img main-img-02'>
+        <ImageView urlAll = {imgUrls} />
+      </picture>
+      <picture className='main-img main-img-03'>
+        <ImageView urlAll = {imgUrls} />
+      </picture>
   </section>
   )
 }
