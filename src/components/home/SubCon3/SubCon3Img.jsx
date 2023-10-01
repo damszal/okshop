553e41190e0d5706e42315177bd7f43d1dced7d1
damian <damian.szalimow@onet.eu>
2023-10-01 17:10:33 +0200

    add centering items and change a tag position

--- src/components/home/SubCon3/SubCon3Img.jsx
+++ src/components/home/SubCon3/SubCon3Img.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 import imgUrl from '../../../assets/items/item3small.png'
 import { Link } from "react-router-dom";
@@ -7,10 +6,10 @@ function SubCon3Img() {
   return (
     <>
     <h2>item name</h2>
-    <img src={imgUrl} alt="" />
-      <button className='show-item-btn'>
-        <Link to="/cart/item">show item</Link>
-      </button>     
+    <img src={imgUrl} alt="image description" />
+      <Link to="/cart/item">
+        <button className='show-item-btn'>show item </button>
+      </Link>    
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facilis commodi suscipit officia maiores provident error molestias maxime tempora ratione!</p>  
     </>
   )
