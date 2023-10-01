3f70e6f6ac071e0d7dbf9f19ed15a4ae86528577
damian <damian.szalimow@onet.eu>
2023-10-01 16:58:19 +0200

    add alt to img tag instead of source tag

--- src/components/home/SubCon1/ImageView.jsx
+++ src/components/home/SubCon1/ImageView.jsx
@@ -5,12 +5,13 @@ function ImageView({urlAll, className, alt}) {
   return (
       <Link to="/cart/item" className={className}>
         <picture >
-          <source media="(min-width: 1024px)" srcSet={urlAll.urlB} alt={alt}/>
-          <source media="(min-width: 768px)" srcSet={urlAll.urlC} alt={alt}/>
-          <source media="(min-width: 640px)" srcSet={urlAll.urlD} alt={alt}/>
-          <source media="(min-width: 475px)" srcSet={urlAll.urlE} alt={alt}/>
+          <source media="(min-width: 1024px)" srcSet={urlAll.urlB}/>
+          <source media="(min-width: 768px)" srcSet={urlAll.urlC}/>
+          <source media="(min-width: 640px)" srcSet={urlAll.urlD}/>
+          <source media="(min-width: 475px)" srcSet={urlAll.urlE}/>
           <img 
           src={urlAll.urlA} 
+          alt={alt}
           />
         </picture>  
       </Link> 
