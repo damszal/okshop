f21184da00add8542765d8a97ce3e5f6ddea04a7
damian <damian.szalimow@onet.eu>
2023-10-12 16:38:46 +0200

    margin and new l-cont class

--- src/components/header/logotype.jsx
+++ src/components/header/logotype.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 function Logotype() {
   return (
-    <Link className='logotype'> {/* propably <A> tag shouldn't be a logotype */}
+    <Link className='logotype l-cont'> {/* propably <A> tag shouldn't be a logotype */}
       <span className='logotype__item logotype__item1_uper_color-blue'>ok</span>
       <span className='logotype__item logotype__item_rot'>!</span>
       <span className='logotype__item logotype__item_color-orange'>shop</span>     
